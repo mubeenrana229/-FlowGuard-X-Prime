90889984a9e224ac2f49ff518f007d09cc619850
mubeenrana229 <112128445+mubeenrana229@users.noreply.github.com>
2026-03-30 01:51:50 +0400

    Delete deploy.js

--- deploy.js
+++
@@ -1,9 +0,0 @@
-const hre = require("hardhat");
-async function main() {
-  const [deployer] = await ethers.getSigners();
-  const Vault = await ethers.getContractFactory("FlowGuardXPrime");
-  const vault = await Vault.deploy([deployer.address], [deployer.address], 1);
-  await vault.deployed();
-  console.log("FlowGuard X Prime Deployed to:", vault.address);
-}
-main().catch((error) => { console.error(error); process.exitCode = 1; });
