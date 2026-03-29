0997e5985d1df5b3c74674bd84d701a232a46c7f
mubeenrana229 <112128445+mubeenrana229@users.noreply.github.com>
2026-03-30 01:49:40 +0400

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,10 +1,9 @@
-{
-  "name": "flowguard-x-prime",
-  "version": "1.0.0",
-  "description": "Institutional Sovereign Vault",
-  "dependencies": {
-    "@openzeppelin/contracts": "^4.9.0",
-    "dotenv": "^16.0.3",
-    "hardhat": "^2.14.0"
-  }
+const hre = require("hardhat");
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const Vault = await ethers.getContractFactory("FlowGuardXPrime");
+  const vault = await Vault.deploy([deployer.address], [deployer.address], 1);
+  await vault.deployed();
+  console.log("FlowGuard X Prime Deployed to:", vault.address);
 }
+main().catch((error) => { console.error(error); process.exitCode = 1; });
